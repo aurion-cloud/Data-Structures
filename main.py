efb647fac5a13258307c4c54c4c47b0a9098cf2d
aurion-cloud <aurion.howard@cloudreach.com>
2023-06-14 13:37:47 -0400

    list updates

---
+++ main.py
@@ -0,0 +1,12 @@
+# List
+#Create a list of cloud service providers that you are aware of, and print it out
+cloud_providers = ["aws", "gpc", "azure", "oracle"]
+print(cloud_providers)
+
+#"""Print second element of the list ["a", "b", "c", "d", "e"] 
+#Print the last element of this list #
+cloud_providers = ["aws", "gpc", "azure", "oracle"]
+print(cloud_providers[1])
+print(cloud_providers[3])
+
+
